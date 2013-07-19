44a4b0ac402af95e1388138103382eb6271c541d
tlake <tanner.lake@gmail.com>
2013-07-18 19:31:49 -0700

    now it's shorter!

--- solution.py
+++ solution.py
@@ -2,34 +2,28 @@ import math
 
 n = 600851475143
 
-factorlist = []
-primeset = set()
-
 def isprime(p):
-	if p in primeset:
-		return True
 	if p % 2 == 0:
 		return False
 	for d in xrange(3, int(math.sqrt(p)) + 1, 2):
 		if p % d == 0:
 			return False
-	primeset.add(p)
 	return True
 
 def factors(f):
+	factorlist = []
 	for d in xrange(2, int(math.sqrt(f)) + 1):
 		if f % d == 0:
 			if d not in factorlist:
 				factorlist.append(d)
 			if f / d not in factorlist:
 				factorlist.append(f / d)
-			factors(f / d)
+			factorlist.extend(factors(f / d))
+			break
+	factorlist.sort(reverse=True)	
+	return factorlist
 
-
-factors(n)
-factorlist.sort(reverse=True)
-
-for q in factorlist:
+for q in factors(n):
 	if isprime(q):
 		print q
 		break
