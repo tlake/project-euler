3feb941c7499311cd55b3cffcdeef89e033d0e56
tlake <tanner.lake@gmail.com>
2013-07-18 19:20:30 -0700

    made shorter

--- solution.py
+++ solution.py
@@ -5,11 +5,7 @@ sums = 0
 while n3 <= 4000000:
 	if not n3%2:
 		sums = n3 + sums
-		n1 = n2
-		n2 = n3
-		n3 = n1 + n2
-	else:
-		n1 = n2
-		n2 = n3
-		n3 = n1 + n2
+	n1 = n2
+	n2 = n3
+	n3 = n1 + n2
 print sums
