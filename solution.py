9f09e4355e713302620c1123b7d9e586616959f7
tlake <tanner.lake@gmail.com>
2013-07-25 17:19:49 -0700

    Now constructs solution via primes and exponents.

--- solution.py
+++ solution.py
@@ -1,21 +1,30 @@
-top = 10
+import math
 
+top = 20
 
-def find_max(a):
-	b = 1
-	for c in range((a / 2) + 1, a + 1):
-		b = c * b
-	return b
-
-
-def divides_evenly(test):
-	for x in range ((top / 2) + 1, top + 1):
-		if test % x != 0:
+def is_prime(x):
+	if x == 2:
+		return True
+	if x % 2 == 0:
+		return False
+	for y in xrange(3, int(math.sqrt(x)) + 1, 2):
+		if x % y == 0:
 			return False
 	return True
 
+def power_trip(x):
+	if x * x > top:
+		return x
+	y = 2
+	while x ** y <= top:
+		y += 1
+	return (x ** (y - 1))
 
-for t in xrange(top ** 2, find_max(top), top):
-	if divides_evenly(t):
-		print t
-		break
+def main(x):
+	n = 1
+	for y in xrange(2, x + 1):
+		if is_prime(y):
+			n = n * power_trip(y)
+	return n
+
+print main(top)
