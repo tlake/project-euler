86f88667568b599913b1588cfa7e37ee814cd054
tlake <tanner.lake@gmail.com>
2013-09-10 19:02:48 -0700

    Made nth more versatile, includes notes and relics.

--- solution.py
+++ solution.py
@@ -1,21 +1,23 @@
 import math, itertools
 
 
-def main(desired_prime):
-	return nth(desired_prime)
-
-
-def nth(desired_prime):
-	generator_title = prime_gen()
+def nth(desired_prime, iterable):
+	the_generator = iterable
 	for x in range(desired_prime - 1):
-		next(generator_title)
-	return next(generator_title)
-
+		next(the_generator)
+	return next(the_generator)
 
 def prime_gen():
-	for x in itertools.count(2):
-		if is_prime(x):	
-			yield x
+	return (x for x in itertools.count(2) if is_prime(x))	
+
+	# This is just sugar for
+	#	for x in itertools.count(2):
+	#		if is_prime(x):	
+	#			yield x
+	# The round brackets make a generator,
+	# square brackets would make a list.
+	# A list here would be bad. It would be infinitely big.
+	# Lol.
 	
 
 def is_prime(test):
@@ -27,3 +29,10 @@ def is_prime(test):
 		if test % d == 0:
 			return False
 	return True
+
+
+def nth_prime(desired_prime):
+	generator_title = prime_gen()
+	for x in range(desired_prime - 1):
+		next(generator_title)
+	return next(generator_title)
